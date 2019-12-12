2402f2e8a5e82b3a52dd4dbdd97bc1ead0659113
Mihkel1996 <mihkeljohannes.paimla@gmail.com>
2019-12-12 17:12:28 +0200

    12.12.2019

--- Controllers/ReviewsController.cs
+++ Controllers/ReviewsController.cs
@@ -9,21 +9,22 @@ namespace OdeToFood.Controllers
 {
     public class ReviewsController : Controller
     {
+        private OdeToFoodDb db = new OdeToFoodDb();
         [ChildActionOnly]
         public ActionResult BestReview()
         {
-
             var bestReview = from r in _reviews
                              orderby r.Rating descending
                              select r;
 
             return PartialView("_Review", bestReview.First());
+
         }
 
 
         //
-        // GET: Reviews
-        public ActionResult Index()
+        // GET: LatestReviews
+        public ActionResult LatestReviews()
         {
             var model =
                 from r in _reviews
@@ -31,77 +32,6 @@ namespace OdeToFood.Controllers
                 select r;
 
             return View(model);
-        }
-
-        // GET: Reviews/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Reviews/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Reviews/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Reviews/Edit/5
-        public ActionResult Edit(int id)
-        {
-            var review = _reviews.Single(r => r.Id == id);
-
-            return View();
-        }
-
-        // POST: Reviews/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            var review = _reviews.Single(r => r.Id == id);
-            if (TryUpdateModel(review))
-            {
-                // ..
-                return RedirectToAction("Index");
-            }
-            return View(review);
-        }
-
-        // GET: Reviews/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Reviews/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
         }
         static List<RestaurantReview> _reviews = new List<RestaurantReview>
         {
@@ -130,5 +60,21 @@ namespace OdeToFood.Controllers
                 Rating = 10
             }
         };
+        public ActionResult Index([Bind(Prefix = "id")] int restaurantId)
+        {
+            var restaurant = db.Restaurants.Find(restaurantId);
+            if (restaurant != null)
+            {
+                return View(restaurant);
+            }
+            return HttpNotFound();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (db!= null) db.Dispose();
+            base.Dispose(disposing);
+        }
     }
 }
+        
--- Models/OdeToFoodDb.cs
+++ Models/OdeToFoodDb.cs
@@ -8,10 +8,7 @@ namespace OdeToFood.Models
 {
     public class OdeToFoodDb : DbContext
     {
-        public OdeToFoodDb() : base("name=DefaultConnection")
-        {
 
-        }
         public DbSet<Restaurant> Restaurants { get; set; }
         public DbSet<RestaurantReview> Reviews { get; set; }
     }
